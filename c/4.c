e08f03b57a5bd638a09d5e6ec4d2ad34fe1fbe14
DESKTOP-T7OUJL7\USER <dalae1009@naver.com>
2024-03-22 14:06:13 +0900

    해달 채점 - c

--- c/4.c
+++ c/4.c
@@ -7,6 +7,18 @@
 
 int main() {
     // 이곳에 코드를 작성해주세요!
+    int result;
+    scanf("%d", &result);
 
+    if (result >= 90)
+        printf("A");
+    else if (result >= 80)
+        printf("B");
+    else if (result >= 70)
+        printf("C");
+    else if (result >= 60)
+        printf("D");
+    else
+        printf("F");
     return 0;
 }
